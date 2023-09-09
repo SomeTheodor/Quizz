8fa0f2de38cc2b8ed4c1edd613c2fb7e184893e6
SomeTheodor <137733932+SomeTheodor@users.noreply.github.com>
2023-09-09 19:15:50 -0300

    Delete changeContent.js

--- changeContent.js
+++
@@ -1,7 +0,0 @@
-const botonVolverAtras = document.getElementById('botonVolverAtras');
-
-if (highestCategory == "Programacion" || highestCategory == "Web" || highestCategory == "Database") {
-    botonVolverAtras.addEventListener('click', function() {
-        window.location.href = 'cont_programacion.html'; // Cambia el enlace para la categoría "programacion" al hacer clic en el botón
-    });
-}
